934cfb54d06f18cbcd3d82e744baff296777f3e6
RattiValerio <valerio.ratti@samtrevano.ch>
2025-01-05 16:22:31 +0100

    changed comments

--- blueprints/api.py
+++ blueprints/api.py
@@ -75,7 +75,7 @@ def calculate_with_drag(lat, lon, m, v0, angle_vertical, angle_horizontal, alt, 
         v = math.sqrt(rel_vx ** 2 + vy ** 2 + rel_vz ** 2)
 
         # Forza di resistenza aerodinamica
-        # R = 1/2 * CR * DAria *
+        # R = 1/2 * CR * DAria * A * v^2
         F_drag = 0.5 * C_r * air_density * A * v ** 2
 
         # Accelerazioni
@@ -108,88 +108,6 @@ def calculate_with_drag(lat, lon, m, v0, angle_vertical, angle_horizontal, alt, 
 
     return (x, y, z), max_height, horizontal_distance, t
 
-# def calculate_with_drag(lat, lon, m, v0, angle_vertical, angle_horizontal, alt, air_data, dt=0.01):
-#     g = 9.81  # Accelerazione di gravità
-#     air_density = float(air_data.get("density") or 1.225)  # Densità dell'aria (kg/m^3)
-#     C_d = 0.47  # Coefficiente di resistenza per una sfera
-#     A = 0.01  # Area frontale del proiettile (m^2)
-#
-#     # Componenti del vento
-#     wind_speed = air_data['wind_speed']
-#     wind_deg = math.radians(air_data['wind_deg'])
-#     wind_vx = wind_speed * math.cos(wind_deg)
-#     wind_vz = wind_speed * math.sin(wind_deg)
-#
-#     # Velocità iniziali
-#     vx = v0 * math.cos(angle_vertical) * math.cos(angle_horizontal)
-#     vy = v0 * math.sin(angle_vertical)
-#     vz = v0 * math.cos(angle_vertical) * math.sin(angle_horizontal)
-#
-#     # Posizione iniziale
-#     x = 0
-#     y = alt
-#     z = 0
-#     t = 0
-#     max_height = y
-#     print(y)
-#
-#     # Lista delle altitudini del terreno lungo la traiettoria
-#     terrain_altitudes = []
-#     i = 0
-#     while y > 0:
-#         # Ottieni l'altitudine del terreno al punto corrente
-#         print(f"N.{i}")
-#         i = i + 1
-#         terrain_altitude = y
-#         if i % 500 == 0:
-#             sleep(0.1)
-#             current_lat, current_lon =  calculate_new_coordinates(lat, lon, y, angle_horizontal)
-#             print(current_lat, current_lon)
-#             terrain_altitude = float(get_altitude(current_lat, current_lon))
-#             print(f"{terrain_altitude}")
-#         print(f"{x} {y} {z}")
-#         terrain_altitudes.append(terrain_altitude)
-#
-#         # Velocità relativa al fluido (vento)
-#         rel_vx = vx - wind_vx
-#         rel_vz = vz - wind_vz
-#         v = math.sqrt(rel_vx ** 2 + vy ** 2 + rel_vz ** 2)
-#
-#         # Forza di resistenza aerodinamica
-#         F_drag = 0.5 * C_d * air_density * A * v ** 2
-#
-#         # Accelerazioni
-#         ax = -F_drag * (rel_vx / v) / m
-#         ay = -g - F_drag * (vy / v) / m
-#         az = -F_drag * (rel_vz / v) / m
-#
-#         # Aggiornamento velocità
-#         vx += ax * dt
-#         vy += ay * dt
-#         vz += az * dt
-#
-#         # Aggiornamento posizioni
-#         x += vx * dt
-#         y += vy * dt
-#         z += vz * dt
-#
-#         # Aggiorna altezza massima
-#         max_height = max(max_height, y)
-#
-#         # Verifica se il proiettile ha toccato il terreno
-#         if y <= terrain_altitude:
-#             break
-#
-#         # Incremento temporale
-#         t += dt
-#
-#
-#
-#     # Calcola distanza orizzontale
-#     horizontal_distance = calculate_horizontal_distance(0, 0, x, z)
-#
-#     return (x, y, z), max_height, horizontal_distance, t
-
 def decimal_to_dms(decimal_coord, coord_type):
     is_negative = decimal_coord < 0
     decimal_coord = abs(decimal_coord)
@@ -204,7 +122,6 @@ def decimal_to_dms(decimal_coord, coord_type):
         direction = 'W' if is_negative else 'E'
 
     return f"{degrees}°{minutes}'{seconds:.2f}\" {direction}"
-
 
 def dms_to_decimal(dms_str):
     deg, minutes, seconds, direction = re.split('[°\'"]', dms_str)
@@ -243,10 +160,8 @@ def calculate_new_coordinates(lat, lon, distance, angle):
 
     return new_lat, new_lon
 
-
 def calculate_horizontal_distance(x1, z1, x2, z2):
     return math.sqrt((x2 - x1) ** 2 + (z2 - z1) ** 2)
-
 
 @app.route('/calculate/projectile_ballistics', methods=['POST'])
 def calculate_projectile_ballistics():
